7f40f40c566cfeb9f1255418cd613e5576f79db1
Dmitriy Kasyanenko <47503436+WAR1US31@users.noreply.github.com>
2020-08-07 15:53:36 +0300

    Переработали вывод новости через DetailView и url

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import News, Categories
 from .forms import NewsForm
 
@@ -49,10 +49,15 @@ class NewsByCategory(ListView):
 #     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # template_name = 'news/news_detail.html'
+    # pk_url_kwarg = 'news_id'
 
 
 def add_news(request):
