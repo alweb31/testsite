fe16fdb95f6280ba1b88f792e896efaf621936f9
Dmitriy Kasyanenko <47503436+WAR1US31@users.noreply.github.com>
2020-08-07 15:39:48 +0300

    Подправили шаблоны вывода статей. Подправили url во News. Переработали вывод статей из конкретной категории.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -8,8 +8,9 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
+
     # extra_context = {'title': 'Главная'}
-    
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
         context['title'] = 'Главная страница'
@@ -17,6 +18,7 @@ class HomeNews(ListView):
 
     def get_queryset(self):
         return News.objects.filter(is_published=True)
+
 
 # def index(request):
 #     news = News.objects.all()
@@ -26,11 +28,25 @@ class HomeNews(ListView):
 #     }
 #     return render(request, 'news/index.html', context=context)
 
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Categories.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Categories.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Categories.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
 def view_news(request, news_id):
