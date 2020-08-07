03e3b813b33a66478d39a85d2be2e6cbfdb99a26
Dmitriy Kasyanenko <47503436+WAR1US31@users.noreply.github.com>
2020-08-07 14:59:38 +0300

    Сделали свой валидатор для title новостей при добавлении

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey('Categories', on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.title
