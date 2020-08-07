1664ccf548eb41fd598504cb78ede245aef5b673
Dmitriy Kasyanenko <47503436+WAR1US31@users.noreply.github.com>
2020-08-07 15:16:57 +0300

    Исправили view News. Добавили фильтр для вывода только опубликованнх записей

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView
 from .models import News, Categories
 from .forms import NewsForm
 
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-    }
-    return render(request, 'news/index.html', context=context)
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    # extra_context = {'title': 'Главная'}
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomeNews, self).get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
+
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, 'news/index.html', context=context)
 
 
 def get_category(request, category_id):
