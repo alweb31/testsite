fec13e23564713081cce605063b1883d87deaf81
Dmitriy <admin@alweb31.site>
2020-08-10 21:20:16 +0300

    Настроили админку. Настроили редактирование новостей в админке

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import News, Categories
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'update_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'update_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'update_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'update_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+
+    get_photo.short_description = 'Миниатюра'
 
 
 class CategoriesAdmin(admin.ModelAdmin):
@@ -18,3 +29,6 @@ class CategoriesAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Categories, CategoriesAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
